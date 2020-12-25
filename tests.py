8dbfd6d03525250d6ab6608765ea648c73a4a526
Vladislada <73122973+Vladislada@users.noreply.github.com>
2020-12-25 17:37:46 +0300

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,77 @@
+
+import unittest
+
+def rabin_karp(text, pattern):
+    result = []
+    pattern_sum = sum(ord(s) for s in pattern)
+    flag = False
+    
+    for i in range(len(text) - len(pattern) + 1):
+        t_part = text[i:(len(pattern) + i)]          
+        text_sum = sum(ord(s) for s in t_part)
+        if pattern_sum == text_sum:                  
+            for j in range(len(t_part) + 1):
+                if t_part[j:j+1] == pattern[j:j+1]:           
+                     flag = True
+            else:
+                flag = False
+                break
+            if flag is True:
+                result.append(i)
+    return result
+
+
+
+class RabinKarpTest(unittest.TestCase):
+    """Тесты для метода Рабина-Карпа"""
+
+    def setUp(self):
+        """Инициализация"""
+        self.text1 = 'axaxaxax'
+        self.pattern1 = 'xax'
+        self.text2 = 'bababab'
+        self.pattern2 = 'bab'
+
+    def test_return_type(self):
+        """Проверка того, что функция возвращает список"""
+        self.assertIsInstance(
+            rabin_karp(self.text1, "x"), list,
+            msg="Функция должна возвращать список"
+        )
+
+    def test_returns_empty(self):
+        """Проверка того, что функция, когда следует, возвращает пустой список"""
+        self.assertEqual(
+            [], rabin_karp(self.text1, "z"),
+            msg="Функция должна возвращать пустой список, если нет вхождений"
+        )
+        self.assertEqual(
+            [], rabin_karp("", self.pattern1),
+            msg="Функция должна возвращать пустой список, если текст пустой"
+        )
+        self.assertEqual(
+            [], rabin_karp("", ""),
+            msg="Функция должна возвращать пустой список, если текст пустой, даже если образец пустой"
+        )
+
+    def test_finds(self):
+        """Проверка того, что функция ищет все вхождения непустых образцов"""
+        self.assertEqual(
+            [1, 3, 5], rabin_karp(self.text1, self.pattern1),
+            msg="Функция должна искать все вхождения"
+        )
+        self.assertEqual(
+            [0, 2, 4], rabin_karp(self.text2, self.pattern2),
+            msg="Функция должна искать все вхождения"
+        )
+
+    def test_finds_all_empties(self):
+        """Проверка того, что функция ищет все вхождения пустого образца"""
+        self.assertEqual(
+            list(range(len(self.text1))), rabin_karp(self.text1, ""),
+            msg="Пустая строка должна находиться везде"
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
